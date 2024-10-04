e340cb8c32552f17e680a2dc114e64cb9bbbdf70
Shoru <bapcui596@gmail.com>
2024-10-04 18:08:38 +0700

    chore!: UI config

---
+++ lib/ui_config.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+// Light Mode UI Configurations
+class LightModeColors {
+  static const Color navBackground = Color(0xFFE3E2E2);
+  static const Color navTextAndIcon = Color(0xFF4A4459);
+  static const Color navSelected = Color(0xFF312D3B);
+
+  static const Color deadlineHeader = Color(0xFF000000);
+  static const Color deadlineClassHeader = Color(0xFF000000);
+  static const Color deadlineContentText = Color(0xFF3C3C43);
+  static const Color deadlineDueText = Color(0xFF666666);
+  static const Color deadlineOverdueText = Color(0xFFFF1515);
+  static const Color deadlinePendingText = Color(0xFFFC850E);
+  static const Color deadlineSubmittedText = Color(0xFF36FC71);
+
+  static const Color commonHeaderText = Color(0xFF000000);
+  static const Color commonFadedText = Color(0xFF3C3C43);
+  static const Color commonDarkText = Color(0xFFFFFFFF);
+
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color buttonCommon = Color(0xFFFFFFFF);
+  static const Color divider = Color(0xFFE6E6E6);
+}
+
+// Dark Mode UI Configurations
+class DarkModeColors {
+  static const Color navBackground = Color(0xFF2E2D2D);
+  static const Color navTextAndIcon = Color(0xFFCFCFCF);
+  static const Color navSelected = Color(0xFF8C8598);
+
+  static const Color deadlineHeader = Color(0xFFFFFFFF);
+  static const Color deadlineClassHeader = Color(0xFFFFFFFF);
+  static const Color deadlineContentText = Color(0xFFBEBEBE);
+  static const Color deadlineDueText = Color(0xFFAAAAAA);
+  static const Color deadlineOverdueText = Color(0xFFFF5A5A);
+  static const Color deadlinePendingText = Color(0xFFFFB74D);
+  static const Color deadlineSubmittedText = Color(0xFF66FF99);
+
+  static const Color commonHeaderText = Color(0xFFFFFFFF);
+  static const Color commonFadedText = Color(0xFFBEBEBE);
+  static const Color commonDarkText = Color(0xFF000000);
+
+  static const Color background = Color(0xFF121212);
+  static const Color buttonCommon = Color(0xFF2C2C2C);
+  static const Color divider = Color(0xFF333333);
+}
